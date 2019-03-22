22689df31de22ca8a9bfd549af7d4bf14dbcd4aa
ChandanSarkar <sarkar1986.chandan@gmail.com>
2019-03-22 16:01:19 +0100

    Added Database as a building block

---
+++ include/Database.hpp
@@ -0,0 +1,16 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+namespace database
+{
+  class Database
+  {
+    #pragma mark Private types
+    private:
+
+    #pragma mark Public initializers
+    public:
+  };
+}
+
+#endif //DATABASE_H

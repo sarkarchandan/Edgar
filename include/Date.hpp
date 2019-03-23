b87b5f2ff2fd5ff6fc97c073929790edcf13ff67
ChandanSarkar <sarkar1986.chandan@gmail.com>
2019-03-23 12:58:28 +0100

    Disabled the days calculation

--- include/Date.hpp
+++ include/Date.hpp
@@ -1,48 +1,117 @@
 #ifndef DATE_H
 #define DATE_H
-#include <iostream>
+#include <cstdlib>
+#include <exception>
+#include <unordered_map>
 
 namespace database
 {
   struct Date
   {
-    #pragma mark Member properties
+    #pragma mark Private member properties
     private:
     int32_t m_day;
     int32_t m_month;
     int32_t m_year;
     int32_t m_serialized_value;
+    std::unordered_map<int32_t,int32_t> m_days_of_months;
 
-    #pragma mark Initializers and accessors
-    public:
+    #pragma mark Private Initializer and implementation layer functions
+    private:
+    inline bool _IsLeapYear(const int32_t& year) const
+    {
+      if((year % 400) == 0) return true;
+      else if((year % 100) == 0) return false;
+      else if((year % 4) == 0) return true;
+      else return false;
+    }
+    void _Validate(const int32_t& year, const int32_t& month, const int32_t& day) const
+    {
+      if(!(month >= 1 && month <= 12))
+        throw std::invalid_argument("Invalid month value");
+      
+      if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) //31 raw_year_days case
+      {
+        if(!(day >= 1 && day <= 31))
+          throw std::invalid_argument("Invalid day value for the given calendar month");
+      }
+      else if(month == 4 || month == 6 || month == 9 || month == 11) //30 raw_year_days case
+      {
+        if(!(day >= 1 && day <= 30))
+          throw std::invalid_argument("Invalid day value for the given calendar month");
+      }
+      else // 28 or 29 raw_year_days case
+      {
+        if(_IsLeapYear(year))
+        {
+          if(!(day >= 1 && day <= 29))
+            throw std::invalid_argument("Invalid day value for the given calendar month of leap year");
+        }else
+          if(!(day >= 1 && day <= 28))
+            throw std::invalid_argument("Invalid day value for the given calendar month");
+      }
+    }
+    void _PopulateDaysOfMonths()
+    {
+      for(int32_t i = 1; i <= 12; i += 1)
+      {
+        if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) m_days_of_months[i] = 31;
+        else if(i == 4 || i == 6 || i == 9 || i == 11) m_days_of_months[i] = 30;
+        else
+        {
+          if(_IsLeapYear(year())) m_days_of_months[i] = 29;
+          else m_days_of_months[i] = 28; 
+        }
+      }
+    }
+    // int32_t _DaysBetweenYears(const int32_t& lhs, const int32_t& rhs) const
+    // {      
+    //   if(lhs == rhs) return 0;
+    //   int32_t raw_year_days = 0;
+    //   if(lhs < rhs)
+    //   {
+    //     for(int32_t i = lhs; i < rhs; i += 1)
+    //     {
+    //       if(_IsLeapYear(i)) raw_year_days += 366;
+    //       else raw_year_days += 365;
+    //     }
+    //   }else
+    //   {
+    //     for(int32_t i = rhs; i < lhs; i += 1)
+    //     {
+    //       if(_IsLeapYear(i)) raw_year_days += 366;
+    //       else raw_year_days += 365;
+    //     }
+    //   }
+    //   return raw_year_days;
+    // }
     Date(const int32_t& year, const int32_t& month, const int32_t& day)
-    :m_year(year),m_month(month),m_day(day)
     {
+      _Validate(year,month,day);
+      m_year = year;
+      m_month = month;
+      m_day = day;
       m_serialized_value = (m_year * 10000) + (m_month * 100) + m_day;
+      _PopulateDaysOfMonths();
     }
-    Date(const int32_t& serialized_value)
-    :m_serialized_value(serialized_value)
-    {
-      m_year = serialized_value / 10000;
-      int32_t year_component_remainder = serialized_value % 10000;
-      m_month = year_component_remainder / 100;
-      m_day = year_component_remainder % 100;
-    }
+
+    public:
+    Date() = delete;
     ~Date(){}
     int32_t day() const { return m_day; }
     int32_t year() const { return m_year; }
     int32_t month() const {return m_month; }
     int32_t serialized_value() const { return m_serialized_value; }
     
-    bool operator == (const database::Date& date) const
+    inline bool operator == (const database::Date& date) const
     {
       return this -> m_year == date.m_year && this -> m_month == date.m_month && this -> m_day == date.m_day;
     }
-    bool operator != (const database::Date& date) const
+    inline bool operator != (const database::Date& date) const
     {
       return !(*this == date);
     }
-    bool operator < (const database::Date& date) const
+    inline bool operator < (const database::Date& date) const
     {
       if(*this == date) return false;
       else
@@ -60,7 +129,7 @@ namespace database
                 if(this -> m_day <= date.m_day)
                 {
                   if(this -> m_day < date.m_day) return true;
-                  else return false;
+                  else return false; //Same day
                 }else return false;
               }
             }else return false;
@@ -69,13 +138,80 @@ namespace database
         else return false;
       }
     }
-    bool operator > (const database::Date& date) const
+    inline bool operator > (const database::Date& date) const
     {
       return (*this != date) && !(*this < date);
+    }
+
+    // int32_t Days_Until(const database::Date& later_date) const
+    // {
+    //   if(later_date < *this)
+    //     throw std::invalid_argument("Argument has an earlier date");
+    //   if(later_date == *this) return 0;
+    //   else
+    //   {
+    //     int32_t raw_month_days = 0;
+    //     int32_t index = month();
+
+    //     while()
+    //     {
+    //       while(index != later_date.month())
+    //       {
+    //         std::cout << "Index: " << index << "\n";
+    //         if(index != 0) raw_month_days += m_days_of_months.at(index);
+    //         index = (index + 1) % 13;
+    //       }
+    //     }
+
+    //     std::cout << "Raw days difference in months: " << raw_difference_months << "\n";
+
+    //     if(month() < later_date.month())
+    //       raw_difference_years += raw_difference_months;
+    //     else 
+    //       raw_difference_years -= raw_difference_months;
+        
+    //     int32_t raw_difference_days = day() - later_date.day();
+    //     return raw_difference_years + raw_difference_days;
+    //   }
+    // }
+
+    // int32_t Days_From(const database::Date& earlier_date) const;
+
+    #pragma mark Static methods to create date
+    //This is because with the default initializers, the relative positions for the components are ambiguous.
+    static Date With_yyyy_mm_dd(const int32_t& year, const int32_t& month, const int32_t& day)
+    { return Date(year,month,day); }
+    static Date With_dd_mm_yyyy(const int32_t& day, const int32_t& month, const int32_t& year)
+    { return Date(year,month,day); }
+    static Date With_mm_dd_yyyy(const int32_t& month, const int32_t& day, const int32_t& year)
+    { return Date(year,month,day); }
+    static Date With_yyyymmdd(const int32_t& serialized_value)
+    {
+      int32_t year = serialized_value / 10000;
+      int32_t year_component_remainder = serialized_value % 10000;
+      int32_t month = year_component_remainder / 100;
+      int32_t day = year_component_remainder % 100;
+      return Date(year,month,day);
+    }
+    static Date With_ddmmyyyy(const int32_t& serialized_value)
+    {
+      int32_t year = serialized_value % 10000;
+      int32_t year_component_remainder = serialized_value / 10000;
+      int32_t month = year_component_remainder % 100;
+      int32_t day = year_component_remainder / 100;
+      return Date(year,month,day);
+    }
+    static Date With_mmddyyyy(const int32_t& serialized_value)
+    {
+      int32_t year = serialized_value % 10000;
+      int32_t year_component_remainder = serialized_value / 10000;
+      int32_t month = year_component_remainder / 100;
+      int32_t day = year_component_remainder % 100;
+      return Date(year,month,day);
     }
   };
 }
 
-// unsigned int32_t operator - (const database& Date& lhs, const database::Date& rhs);
+// int32_t operator - (const database::Date& lhs, const database::Date& rhs);
 
 #endif //DATE_H
--- tests/database_date.cpp
+++ tests/database_date.cpp
@@ -3,52 +3,83 @@
 
 TEST(DateTests,canCreateDates)
 {
-  database::Date date1 = {2017,04,12};
+  database::Date date1 = database::Date::With_yyyy_mm_dd(2017,04,12);
   ASSERT_EQ(date1.day(),12);
   ASSERT_EQ(date1.month(),04);
   ASSERT_EQ(date1.year(),2017);
   ASSERT_EQ(date1.serialized_value(),20170412);
 
-  database::Date date2 = {20031202};
+  database::Date date2 = database::Date::With_yyyymmdd(20031202);
   ASSERT_EQ(date2.day(),02);
   ASSERT_EQ(date2.month(),12);
   ASSERT_EQ(date2.year(),2003);
   ASSERT_EQ(date2.serialized_value(),20031202);
+
+  database::Date date3 = database::Date::With_mm_dd_yyyy(10,05,1998);
+  ASSERT_EQ(date3.day(),05);
+  ASSERT_EQ(date3.month(),10);
+  ASSERT_EQ(date3.year(),1998);
+  ASSERT_EQ(date3.serialized_value(),19981005);
+
+  database::Date date4 = database::Date::With_mmddyyyy(7281986);
+  ASSERT_EQ(date4.day(),28);
+  ASSERT_EQ(date4.month(),7);
+  ASSERT_EQ(date4.year(),1986);
+  ASSERT_EQ(date4.serialized_value(),19860728);
+
+  database::Date date5 = database::Date::With_dd_mm_yyyy(1,11,2001);
+  ASSERT_EQ(date5.day(),1);
+  ASSERT_EQ(date5.month(),11);
+  ASSERT_EQ(date5.year(),2001);
+  ASSERT_EQ(date5.serialized_value(),20011101);
+
+  database::Date date6 = database::Date::With_ddmmyyyy(19022014);
+  ASSERT_EQ(date6.day(),19);
+  ASSERT_EQ(date6.month(),02);
+  ASSERT_EQ(date6.year(),2014);
+  ASSERT_EQ(date6.serialized_value(),20140219);
+}
+
+TEST(DateTests_ExceptionTests,canValidate)
+{
+  EXPECT_THROW(database::Date::With_ddmmyyyy(32012017),std::invalid_argument);
+  EXPECT_THROW(database::Date::With_mmddyyyy(13052008),std::invalid_argument);
+  EXPECT_THROW(database::Date::With_yyyymmdd(20030631),std::invalid_argument);
 }
 
 TEST(DateTests,canCompareDates)
 {
-  database::Date date1 = {2017,01,02};
-  database::Date date2 = {20170103};
+  database::Date date1 = database::Date::With_yyyy_mm_dd(2017,01,02);
+  database::Date date2 = database::Date::With_yyyymmdd(20170103);
   ASSERT_TRUE(date1 < date2);
   ASSERT_TRUE(date2 > date1);
   
-  database::Date date3 = {2016,02,18};
-  database::Date date4 = {2016,01,19};
+  database::Date date3 = database::Date::With_yyyy_mm_dd(2016,02,18);
+  database::Date date4 = database::Date::With_ddmmyyyy(19012016);
   ASSERT_TRUE(date4 < date3);
   ASSERT_TRUE(date3 > date4);
 
-  database::Date date5 = {20180323};
-  database::Date date6 = {2019,04,23};
+  database::Date date5 = database::Date::With_ddmmyyyy(23032018);
+  database::Date date6 = database::Date::With_yyyymmdd(20190423);
   ASSERT_TRUE(date5 < date6);
   ASSERT_TRUE(date6 > date5);
 
-  database::Date date7 = {20180212};
-  database::Date date8 = {2018,02,12};
+  database::Date date7 = database::Date::With_yyyymmdd(20181202);
+  database::Date date8 = database::Date::With_mmddyyyy(12022018);
   ASSERT_TRUE(date7 == date8);
 
-  database::Date date9 = {2019,02,23};
-  database::Date date10 = {2018,02,23};
+  database::Date date9 = database::Date::With_mm_dd_yyyy(02,23,2019);
+  database::Date date10 = database::Date::With_dd_mm_yyyy(23,02,2018);
   ASSERT_TRUE(date9 != date10);
   ASSERT_TRUE(date9 > date10);
 
-  database::Date date11 = {20171123};
-  database::Date date12 = {20171023};
+  database::Date date11 = database::Date::With_yyyymmdd(20171123);
+  database::Date date12 = database::Date::With_ddmmyyyy(23102017);
   ASSERT_TRUE(date11 != date12);
   ASSERT_TRUE(date12 < date11);
 
-  database::Date date13 = {20170512};
-  database::Date date14 = {2017,05,11};
+  database::Date date13 = database::Date::With_yyyymmdd(20170512);
+  database::Date date14 = database::Date::With_yyyy_mm_dd(2017,05,11);
   ASSERT_TRUE(date13 != date14);
   ASSERT_FALSE(date13 < date14);
 }
